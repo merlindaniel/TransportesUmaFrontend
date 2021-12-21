9ee714d9703ecaade5051c38b774ec6d5e832ec6
Jose Sierra <josiesala@gmail.com>
2021-12-21 20:59:33 +0100

    CRUD perfil, CRUD vehiculos

---
+++ vue/profile.js
@@ -0,0 +1,171 @@
+const app = new Vue({
+    el: '#hero',
+    data: {
+        // Logged user id
+        loggedUserId: '61c1c831437c756c894c1fe4',
+
+        // Current user data:
+        // Example id = 61c1c831437c756c894c1fe4
+        userId: '61c1c831437c756c894c1fe4', // Eliminar id por defecto (ahora está para hacer pruebas)
+        user: {},
+        editionUser: {},
+        vehicles: [],
+        isLoggedUser: false,
+
+        errors: [],
+    },
+    methods: {
+        async getUserId() {
+            const queryString = window.location.search;
+            const urlParams = new URLSearchParams(queryString);
+            const userId = urlParams.get('user');
+
+            if (userId)
+                this.userId = userId;
+        },
+
+        async getUser() {
+            const response = await axios.get('http://localhost:8080/api/users/' + this.userId);
+            this.user = response.data;
+            console.log(this.user);
+            
+            // Checks if this user is the current logged user
+            this.isLoggedUser = this.user.id === this.loggedUserId;
+
+            if (this.isLoggedUser)
+                Object.assign(this.editionUser, this.user);
+        },
+
+        async getVehicles() {
+            const response = await axios.get('http://localhost:8080/api/vehicles/user/' + this.userId);
+            this.vehicles = response.data;
+            console.log(this.vehicles);
+        },
+
+        async userEditionSubmit() {
+            let canEdit = await this.editionValidation();
+
+            if (canEdit) {
+                let imgUrl = this.user.picture;
+                const img = document.getElementById("profile-pic");
+    
+                // If image was uploaded, then uploads image to imgur
+                if (img.files.length > 0) {
+                    const formdata = new FormData();
+                    formdata.append("image", img.files[0]);
+                    await fetch("https://api.imgur.com/3/image/", {
+                        method: "post",
+                        headers: {
+                            Authorization: "Client-ID 4f59459848bf8ef"
+                        },
+                        body: formdata
+                    }).then(data => data.json()).then(data => {
+                        imgUrl = data.data.link;
+
+                        console.log(imgUrl);
+                    });
+                }
+
+                this.editionUser.picture = imgUrl;
+
+                console.log("Edited user: ");
+                console.log(this.editionUser);
+
+                const response = await axios.put('http://localhost:8080/api/users/' + this.user.id, this.editionUser);
+
+                console.log("Response:");
+                console.log(response);
+                
+                // Loads profile page after applying changes
+                window.location.href = './profile.html';
+            }
+        },
+
+        async editionValidation() {
+            // Reiniciar mensajes de error
+            this.errors = [];
+
+            this.validateUsername();
+            this.validateEmail();
+            this.validateName();
+            this.validatePassword();
+
+            console.log(this.errors);
+            return this.errors.length === 0;
+        },
+
+        async validateUsername() {
+            // Comprobar que el nombre de usuario no está en uso o es igual al actual
+            const editionUsername = document.getElementById('edition-username').value;
+            if (editionUsername.length < 3 || editionUsername.length > 20) {
+                this.errors.push(`El nombre de usuario "${editionUsername}" no es válido`);
+            }
+            else if (editionUsername !== this.user.username) {
+                let response;
+                response = await axios.get('http://localhost:8080/api/users/' + editionUsername).catch(error => {
+                    return {};
+                });
+
+                if (response && response.data) {
+                    this.errors.push(`El nombre de usuario "${editionUsername}" no está disponible`);
+                    console.log(this.errors);
+                }
+            }
+        },
+
+        async validateEmail() {
+            // Comprobar que el email del usuario no está en uso o es igual al actual
+            const editionEmail = document.getElementById('edition-email').value;
+            if (!editionEmail.includes('@') || !editionEmail.includes('.')) {
+                this.errors.push(`El email "${editionEmail}" no es un email válido`);
+            }
+            else if (editionEmail !== this.user.email) {
+                let response = await axios.get('http://localhost:8080/api/users/' + editionEmail);
+                if (response && response.data) {
+                    this.errors.push(`El email "${editionEmail}" no está disponible`);
+                    console.log(this.errors);
+                }
+            }
+        },
+
+        async validateName() {
+            // Comprobar que el nombre del usuario es válido
+            const editionName = document.getElementById('edition-name').value;
+            if (editionName.length < 1 || editionName.length > 100) {
+                this.errors.push(`El nombre "${editionName}" no es válido`);
+            }
+        },
+
+        async validatePassword() {
+            // Comprobar que el email del usuario no está en uso o es igual al actual
+            const editionPassword = document.getElementById('edition-password').value;
+            const editionPasswordRepeat = document.getElementById('edition-password-repeat').value;
+
+            if (editionPassword !== editionPasswordRepeat || editionPassword.length < 3) {
+                this.errors.push(`Las contraseñas no coinciden o no son válidas`);
+            }
+        },
+
+        async deleteUser() {
+            if (confirm("¿Estás seguro de que quieres eliminar tu cuenta? Esta acción no se puede deshacer.")) {
+                const response = await axios.delete('http://localhost:8080/api/users/' + this.user.id);
+                console.log(response);
+                window.location.href = './index.html';
+            }
+        },
+
+        async updateImg() {
+            var fReader = new FileReader();
+            fReader.readAsDataURL(document.getElementById("profile-pic").files[0]);
+            fReader.onloadend = function(event) {
+                var img = document.getElementById("picture");
+                img.src = event.target.result;
+            }
+        }
+    },
+    mounted: function (){
+        this.getUserId();
+        this.getUser();
+        this.getVehicles();
+    },
+});
---
+++ vue/vehicle.js
@@ -0,0 +1,157 @@
+new Vue({
+    el: '#hero',
+    data: {
+        // Logged user id
+        loggedUserId: '61c1c831437c756c894c1fe4',
+        isLoggedUser: false,
+
+        editing: false,
+        vehicleId: null,
+        vehicle: {},
+        editionVehicle: {},
+
+        errors: [],
+    },
+    methods: {
+        async getVehicleId() {
+            const queryString = window.location.search;
+            const urlParams = new URLSearchParams(queryString);
+            const vehicle = urlParams.get('vehicle');
+
+            if (vehicle) {
+                this.vehicleId = vehicle;
+                this.editing = true;
+            } else {
+
+                // Default values
+                this.editionVehicle.seats = 2;
+                this.editionVehicle.owner = this.loggedUserId;
+                this.editionVehicle.combustible = "GASOLINE";
+                this.editionVehicle.name = "Mi vehículo";
+
+                console.log(this.editionVehicle);
+            }
+        },
+
+        async getVehicle() {
+            if (this.editing) {
+                const response = await axios.get('http://localhost:8080/api/vehicles/' + this.vehicleId);
+                this.vehicle = response.data;
+                console.log(this.vehicle);
+                
+                // Checks if this user is the current logged user
+                this.isLoggedUser = this.loggedUserId && this.vehicle.owner === this.loggedUserId;
+    
+                if (this.isLoggedUser)
+                    Object.assign(this.editionVehicle, this.vehicle);
+                
+            } else if (this.loggedUserId) {
+                this.isLoggedUser = true;
+            }
+        },
+        
+        async vehicleSubmit() {
+            let canSubmit = await this.fieldsValidation();
+
+            if (canSubmit) {
+                let imgUrl = this.vehicle.picture;
+                const img = document.getElementById("vehicle-pic");
+    
+                // If image was uploaded, then uploads image to imgur
+                if (img.files.length > 0) {
+                    const formdata = new FormData();
+                    formdata.append("image", img.files[0]);
+                    await fetch("https://api.imgur.com/3/image/", {
+                        method: "post",
+                        headers: {
+                            Authorization: "Client-ID 4f59459848bf8ef"
+                        },
+                        body: formdata
+                    }).then(data => data.json()).then(data => {
+                        imgUrl = data.data.link;
+
+                        console.log(imgUrl);
+                    });
+                }
+
+                this.editionVehicle.picture = imgUrl;
+
+                // Set owner to logged user
+                if (!this.editing) {
+                    this.editionVehicle.owner = this.loggedUserId;
+                }
+
+                console.log("Edited vehicle: ");
+                console.log(this.editionVehicle);
+
+                let response;
+                if (this.editing) {
+                    response = await axios.put('http://localhost:8080/api/vehicles/' + this.vehicle.id, this.editionVehicle);
+                } else {
+                    response = await axios.post('http://localhost:8080/api/vehicles/', this.editionVehicle);
+                }
+
+                console.log("Response:");
+                console.log(response);
+                
+                // Loads profile page after applying changes
+                window.location.href = './profile.html?user=' + this.loggedUserId;
+            }
+        },
+
+        async fieldsValidation() {
+            // Reiniciar mensajes de error
+            this.errors = [];
+
+            // Model validation
+            if (!this.editionVehicle.model || this.editionVehicle.model.length < 3) {
+                this.errors.push(`El modelo "${this.editionVehicle.model}" no es válido.`);
+            }
+
+            // Registration validation
+            if (!this.editionVehicle.registration || this.editionVehicle.registration.length < 3) {
+                this.errors.push(`El número de registro "${this.editionVehicle.registration}" no es válido.`);
+            }
+
+            // Name validation
+            if (!this.editionVehicle.name || this.editionVehicle.name.length < 3) {
+                this.errors.push(`El nombre "${this.editionVehicle.name}" no es válido.`);
+            }
+
+            // Seats validation
+            if (!this.editionVehicle.seats || this.editionVehicle.seats < 2) {
+                this.errors.push(`El número de asientos "${this.editionVehicle.seats}" no es válido.`);
+            }
+
+            // Combustible validation
+            if (!this.editionVehicle.combustible) {
+                this.errors.push(`El tipo de combustible "${this.editionVehicle.combustible}" no es válido.`);
+            }
+
+            console.log(this.errors);
+            return this.errors.length === 0;
+        },
+
+        
+        async updateImg() {
+            var fReader = new FileReader();
+            fReader.readAsDataURL(document.getElementById("vehicle-pic").files[0]);
+            fReader.onloadend = function(event) {
+                var img = document.getElementById("picture");
+                img.src = event.target.result;
+            }
+        },
+
+        async deleteVehicle() {
+            if (confirm("¿Estás seguro de que quieres eliminar este vehículo? Esta acción no se puede deshacer.")) {
+                const response = await axios.delete('http://localhost:8080/api/vehicles/' + this.vehicle.id);
+                console.log(response);
+                window.location.href = './profile.html?user=' + this.loggedUserId;
+            }
+        },
+    },
+    created: function () {
+        this.getVehicleId();
+        this.getVehicle();
+    },
+});
