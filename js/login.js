d0d940a93a100055f59fe321340ecc1cff27e8af
merlindaniel <merlindaniel30@gmail.com>
2021-12-26 13:33:28 +0100

    Login implemented

--- js/login.js
+++ js/login.js
@@ -1,19 +1,15 @@
-let backendPaths = {
-    preffix: "http://localhost:8080",
-    pathAuthenticationLogin: "/authentication/user/login"
-}
-
-
-
 let app = new Vue({
     el: '#appVue',
     data: {
         username: "",
         password: "",
+        error: false,
         data: null
     },
     methods: {
-        async logIn(){
+        async logIn(e){
+            e.preventDefault();
+            
             let response = await fetch(backendPaths.preffix + backendPaths.pathAuthenticationLogin, {
                     method: 'POST',
                     headers: {
@@ -26,10 +22,13 @@ let app = new Vue({
                 });
 
             if(response.status == 200){
+                this.error = false;
                 this.data = await response.json();
                 Vue.$cookies.set('Token', this.data.jwttoken);
+                window.location.href = frontendPaths.pathIndex;
             } else {
                 //Datos incorrectos
+                this.error = true;
             }
         }
     }
---
+++ js/paths.js
@@ -0,0 +1,9 @@
+let backendPaths = {
+    preffix: "http://localhost:8080",
+    
+    pathAuthenticationLogin: "/authentication/user/login"
+}
+
+let frontendPaths = {
+    pathIndex: "/index.html"
+}
