5c7629a47b52bd8c65e704701171a6d2d8909faa
merlindaniel <merlindaniel30@gmail.com>
2021-12-26 15:30:10 +0100

    Login.js

--- js/login.js
+++ js/login.js
@@ -8,23 +8,23 @@ let app = new Vue({
     },
     methods: {
         async logIn(e){
+            let v = Vue.$cookies.get('TokenJWT');
             e.preventDefault();
-            
             let response = await fetch(backendPaths.preffix + backendPaths.pathAuthenticationLogin, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                        username: this.username,
-                        password: this.password
-                    })
-                });
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    username: this.username,
+                    password: this.password
+                })
+            });
 
             if(response.status == 200){
                 this.error = false;
                 this.data = await response.json();
-                Vue.$cookies.set('Token', this.data.jwttoken);
+                Vue.$cookies.set('TokenJWT', this.data.jwttoken);
                 window.location.href = frontendPaths.pathIndex;
             } else {
                 //Datos incorrectos
