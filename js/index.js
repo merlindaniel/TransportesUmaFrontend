2ed9aadfa29d3583b13e459a6c1be94a70570af3
JavierSantiburcio <36241745+JavierSantiburcio@users.noreply.github.com>
2022-01-09 20:18:14 +0100

    Corrección de bugs y conexión de index con la búsqueda

--- js/index.js
+++ js/index.js
@@ -2,9 +2,59 @@ const app = new Vue({
 
     el: '#app',
     data: {
-        tokenConBearer: null
+        tokenConBearer: null,
+        origin: '',
+        destination: '',
+        nSpots: '',
+        date: '',
+        listOrigin: [], 
+        listDestination:  [],
+        listJourney: []
     },
+
     methods: {
+        async getListOrigin(){
+
+            let response = await fetch('http://localhost:8080/api/opendata/get/places/40.416729/-3.703339/'+ this.origin, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization':this.tokenConBearer
+                }});
+    
+            if(response.ok){
+                // Devuelve la lista con posibles origenes
+                this.listOrigin = await response.json();
+            } else {
+                // Muestra un error
+    
+            }
+    
+        },
+
+        async getListDestination(){
+
+            let response = await fetch('http://localhost:8080/api/opendata/get/places/40.416729/-3.703339/'+ this.destination, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization':this.tokenConBearer
+                }});
+    
+            if(response.ok){
+                // Devuelve la lista con posibles destinos
+                this.listDestination = await response.json();
+            } else {
+                // Muestra un error
+    
+            }
+    
+        },
+        
+        async searchJourney(){
+       
+            window.location.href = frontendPaths.pathJourneys + "?origin=" + this.origin + "&destination=" 
+            + this.destination + "&nSpots=" + this.nSpots + "&date=" + this.date;
+
+        },
         obtenerTokenBearer() {
             this.tokenConBearer = Vue.$cookies.get('TokenJWT');
         }
