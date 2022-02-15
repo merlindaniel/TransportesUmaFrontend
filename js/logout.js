d495ebb5e99df7492ae8a6f2e5a8049c0073905e
merlindaniel <merlindaniel30@gmail.com>
2022-02-15 18:52:42 +0100

    Se añade cierre de sesion con OAuth a todas las paginas

--- js/logout.js
+++ js/logout.js
@@ -1,18 +1,17 @@
 let btnLogOut = document.getElementById('logoutBtn');
-if(btnLogOut!==null){
-    btnLogOut.onclick = () => {
-        Vue.$cookies.remove('TokenJWT');
+if (btnLogOut !== null) {
+  btnLogOut.onclick = () => {
+    Vue.$cookies.remove('TokenJWT');
 
-        try {
-          let auth2 = gapi.auth2.getAuthInstance();
-          auth2.signOut().then(function () {
-              console.log('User signed out.');
-              window.location.href = './index.html';
-          });
-        } catch (error){
-          window.location.href = './index.html';
-        }
-        
-      }
+    try {
+      let auth2 = gapi.auth2.getAuthInstance();
+      auth2.signOut().then(function () {
+        console.log('User signed out.');
+        window.location.href = './index.html';
+      });
+    } catch (error) {
+      window.location.href = './index.html';
+    }
+
+  }
 }
-    
